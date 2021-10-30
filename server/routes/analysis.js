7bdfd099d3d81dd67f5dfca7cbde92bb4d096ee2
Max Huang <40913223+maxhuang@users.noreply.github.com>
2021-10-30 18:52:13 +1000

    Updated analysis route to match with new requirements

--- server/routes/analysis.js
+++ server/routes/analysis.js
@@ -11,16 +11,25 @@ router.get("/", (req, res) => {
 router.post("/", express.json(), (req, res) => {
 //router.post("/:mode", express.text(), (req, res) => {
     // Error if no body was posted
-    if (!req.body?.data) {
+    if (!req.body?.data || !Array.isArray(req.body.data)) {
         res.status(400).json({
             "status": false,
-            "msg": "body is empty"
+            "msg": "invalid body"
         });
         return;
     }
 
     // Analyse the sentiment of the text
-    let result = new sentiment().analyze(req.body.data);
+    //let result = new sentiment().analyze(req.body.data);
+    let result = req.body.data.map((post) => {
+        let postText = post?.title + post?.content;
+        if (typeof postText === "string") {
+            return new sentiment().analyze(postText).comparative;
+        } else {
+            console.error("invalid post encountered")
+            return undefined;
+        }
+    });
 
     // Check if we need to be inefficient
     if (req.body?.mode == "inefficient") {
